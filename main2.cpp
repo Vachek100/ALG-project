80bf09b8a72f508de5ae650a8c5df40d6ced5cd9
Vaclav Kralik <vaclav.kralik.1610@gmail.com>
2024-04-15 22:25:50 +0200

    created main2.cpp and added code in it

---
+++ main2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void create(vector<double>& A){
+    int n; //variable to store number of terms in the polynomial
+    double coeff;//variable to store the coefficient
+
+    cout << "Enter the number of terms: ";
+    cin >> n;
+
+    A.resize(n);
+
+    for(int i = 0; i < n; i++){
+        cout << "Enter the coefficient for term " << i + 1 << ": ";
+        cin >> coeff; 
+        A[i] = coeff;
+    }
+}
+
+void print(const vector<double>& A){
+    if(A.empty()){
+        cout << "No Polynomial.\n";
+    }else{
+        for(int i = A.size() - 1; i >= 0; --i){
+            if(A[i] != 0){
+                cout << A[i] << "x^" << i;
+                if(i > 0){
+                    cout << " + ";
+                }
+            }
+        }
+        cout << "\n";
+    }
+}
+
+int main(){
+    vector<double> A; // Polynomial represented as a vector of coefficients
+    cout << "Enter the polynomial:\n";
+    create(A);
+    print(A);
+    return 0;
+}
